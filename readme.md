283e9fa897b94308a12b5e88d3c92a741d08cbbc
Arash Kh <106018197+ArashkKH@users.noreply.github.com>
2025-05-15 04:40:58 -0400

    updated W00 - baby steps

--- readme.md
+++ readme.md
@@ -4,6 +4,4 @@ This is Arash's Object Oriented Programming repository
 - STID: 146731245
 - Name: Arash KH.
 - SenecaEmail: akheirollahi@myseneca.ca
-- Subject and section: OOP244NAA
-
-
+- Subject and section: OOP244NFF
---
+++ workshops/W00/w0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "#### Workshop Zero ####" << endl;
+    cout << "Subject and Section:" << endl;
+    cout << "OOP244 NFF" << endl;
+    cout << "Name:" << endl;
+    cout << "Arash Kheirollahi" << endl;
+    cout << "Student Number:" << endl;
+    cout << "146731245" << endl;
+    cout << "Email:" << endl;
+    cout << "akheirollahi@myseneca.ca" << endl;
+
+    return 0;
+}
