7429f9666c6b8f3415c221b42856499a01bf6d49
Arash Kh <106018197+ArashkKH@users.noreply.github.com>
2025-05-22 00:54:21 -0400

    init

---
+++ workshops/W01/w1.cpp
@@ -0,0 +1,21 @@
+/* Citation and Sources...
+-----------------------------------------------------------
+Workshop 01 
+Module: mainFile
+Filename: w1.cpp
+-----------------------------------------------------------
+Author: Arash Kheirollahi
+Student number: 146,731,245
+Email: akheirollahi@myseneca.ca
+Subject: OOP244NFF
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.1    2025/5/22 Workshop Initiation
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
+
