c96953bc63afe24b88b850a06c76bd7bd496866c
Arash Kh <106018197+ArashkKH@users.noreply.github.com>
2025-07-24 14:31:42 -0400

    Revert "Revert Me"
    This reverts commit 0bf4d4a1a7459c69a66d8bfb4bb3b725c4d076b8.

---
+++ workshops/W08/lab/Line.h
@@ -0,0 +1,39 @@
+/**
+ *  OOP244 Workshop 8 lab: Labeld line class (Header)
+ *  File	Line.cpp
+ *  Version 1.0
+ *  Author	Arash Kheirollahi
+ *  Description:
+ *      Encapsulates a horizontal line on a screen with a label
+ *      (This concrete class draws a labeled line)
+ *      
+ *  Revision History:
+ *  -----------------------------------------------------------
+ *  Name            Date            Reason
+ *  0.012           2025-07-24      Workshop submission
+ ___________________________________________________________ */
+#ifndef SENECA_LINE_H
+#define SENECA_LINE_H
+
+#include "LblShape.h"
+
+
+namespace seneca {
+
+   class Line : public LblShape{
+       // Length of the line
+      int m_length = 0;
+   public:
+      // Default constructor
+      Line() = default;
+      // Constructor that initializes label and length
+      Line(const char* label, int len);
+      // Draws the line, using the label and length
+      void draw(ostream& os) const override;
+      // Gets specifications of the line (label and length) from input
+      void getSpecs(istream& is) override;
+   };
+
+}
+
+#endif // !SENECA_LINE_H
