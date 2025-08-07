8a5f4611110e35ee8c762e6235884b91711b559f
Arash Kh <106018197+ArashkKH@users.noreply.github.com>
2025-08-07 17:42:31 -0400

    Update dynaCopy.h

---
+++ workshops/W10/lab/dynaCopy.h
@@ -0,0 +1,65 @@
+/**
+ *  OOP244 Workshop 10 lab: DynaCopy
+ *  File	dynacopy.cpp
+ *  Version 1.0
+ *  Author Arash Kheirollahi
+ *  Description:
+ *      The header file for DynaCopy functions
+ *      
+ *  Revision History:
+ *  -----------------------------------------------------------
+ *  Name            Date            Reason
+ *  0.01           2025-08-7      workshop submission
+ ___________________________________________________________ */
+
+
+#if !defined(DYNACOPY_H)
+#define DYNACOPY_H
+#include <iostream>
+using std::cout;
+using std::endl;
+
+ 
+ namespace seneca{
+     
+    template <typename T>
+    T* dynaCopy(T*& ref , const T* src , int size);
+
+
+    template <typename T>
+    T* dynaCopy(const T* src , int size){
+        T* newArr = nullptr;
+        dynaCopy(newArr , src , size);
+        return newArr;
+    }
+
+    template <typename T>
+    T* dynaCopy(T*& ref , const T* src , int size){
+        delete [] ref;
+        ref = nullptr;
+
+        if(src){
+            ref = new T[size];
+            for (int i = 0; i < size; i++)
+            {
+                ref[i] = src[i];
+            }
+        }
+        return ref;        
+    }
+
+    template <typename T>
+    void prnArray(const T* src , int size){
+        for (int i = 0; i < size; i++)
+        {
+            cout << src[i];
+            if (i < size - 1) cout << ",";
+        }
+
+        cout << endl;  
+    }
+
+ }
+ 
+ #endif // DYNACOPY_H
+ 
